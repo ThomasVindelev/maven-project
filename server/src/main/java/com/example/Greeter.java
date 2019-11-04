6ce40b537bd8000ff847d059753af82858515300
ThomasVindelev <thom891b@stud.kea.dk>
2019-11-04 09:19:16 +0100

    Commit 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,13 @@ public class Greeter {
   }
 
   /**
-   * This is a method
+   * This is a method.
    * @param someone
-   * @return
+   * Hello
+   * @return String
+   * Hello yo
    */
-  public final String greet(final String someone) {
+  public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
